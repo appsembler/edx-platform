c1024c5df0c3bd8b21b1dbced84486c7f647ec85
Bryan Wilson <bryanlandia@gmail.com>
2019-03-22 00:48:26 -0700

    use Badgr for badge sharing modal from certs banner

--- lms/djangoapps/certificates/views/webview.py
+++ lms/djangoapps/certificates/views/webview.py
@@ -444,12 +444,14 @@ def _update_badge_context(context, course, user):
     """
     Updates context with badge info.
     """
-    badge = None
-    if badges_enabled() and course.issue_badges:
-        badges = get_completion_badge(course.location.course_key, user).get_for_user(user)
-        if badges:
-            badge = badges[0]
-    context['badge'] = badge
+    context['badge'] = None
+    if badges_enabled() course.issue_badges:
+        badge_class = get_completion_badge(course.id, user)
+        if badge_class:
+            badges = badge_class.get_for_user(user)
+            if badges:
+                context['badge'] = badges[0]
+                context['badge_meta'] = {'badges_backend_public_url': settings.BADGR_PUBLIC_URL}
 
 
 def _update_organization_context(context, course):
