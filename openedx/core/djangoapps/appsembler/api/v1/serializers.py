8abe1802255dba8b0668fcafcf8970d8bb343e1b
John Baldwin <jlbaldwin@gmail.com>
2019-06-19 19:53:41 -0400

    Tahoe API serializer - Add BulkEnrollmentSerializer

--- openedx/core/djangoapps/appsembler/api/v1/serializers.py
+++ openedx/core/djangoapps/appsembler/api/v1/serializers.py
@@ -1,7 +1,14 @@
 
 from rest_framework import serializers
 
+from opaque_keys import InvalidKeyError
+from opaque_keys.edx.keys import CourseKey
+
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+
+
+class StringListField(serializers.ListField):
+    child = serializers.CharField()
 
 
 class CourseOverviewSerializer(serializers.ModelSerializer):
@@ -11,3 +18,29 @@ class CourseOverviewSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'display_name', 'org',
         )
+
+
+class BulkEnrollmentSerializer(serializers.Serializer):
+    identifiers = StringListField()
+    courses = StringListField()
+    action = serializers.ChoiceField(
+        choices=(
+            ('enroll', 'enroll'),
+            ('unenroll', 'unenroll')
+        ),
+        required=True
+    )
+    auto_enroll = serializers.BooleanField(default=False)
+    email_learners = serializers.BooleanField(default=False)
+
+    def validate_courses(self, value):
+        """
+        Check that each course key in list is valid.
+        """
+        course_keys = value
+        for course in course_keys:
+            try:
+                CourseKey.from_string(course)
+            except InvalidKeyError:
+                raise serializers.ValidationError("Course key not valid: {}".format(course))
+        return value
