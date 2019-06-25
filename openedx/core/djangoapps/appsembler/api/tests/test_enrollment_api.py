fa47a32185d9a4d4b921424adda7db7f4608757f
John Baldwin <jlbaldwin@gmail.com>
2019-06-25 00:16:30 -0400

    WIP Tahoe enrollment api test

--- openedx/core/djangoapps/appsembler/api/tests/test_enrollment_api.py
+++ openedx/core/djangoapps/appsembler/api/tests/test_enrollment_api.py
@@ -5,11 +5,12 @@ These tests adapted from Appsembler enterprise `appsembler_api` tests
 
 """
 from django.contrib.sites.models import Site
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
 from django.test.utils import override_settings
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from rest_framework.test import APIRequestFactory, force_authenticate
 
 import ddt
 import mock
@@ -141,6 +142,7 @@ class EnrollmentApiTest(ModuleStoreTestCase):
         res = self.client.post(url, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
+    @skip('needs user')
     def test_enroll_learners_single_course(self):
         """
         The payload structure is subject to change
@@ -167,3 +169,93 @@ class EnrollmentApiTest(ModuleStoreTestCase):
 
         # TODO: Assert that the course enrollments created
         # TODO: Assert new users registered
+        import pdb; pdb.set_trace()
+
+
+@ddt.ddt
+# @mock.patch(APPSEMBLER_API_VIEWS_MODULE + '.EnrollmentViewSet.authentication_classes', [])
+# @mock.patch(APPSEMBLER_API_VIEWS_MODULE + '.EnrollmentViewSet.permission_classes', [AllowAny])
+@mock.patch(APPSEMBLER_API_VIEWS_MODULE + '.EnrollmentViewSet.throttle_classes', [])
+class EnrollmentApiPostTest(ModuleStoreTestCase):
+
+    def setUp(self):
+        super(EnrollmentApiPostTest, self).setUp()
+        # store = modulestore()
+        self.my_site = Site.objects.get(domain=u'example.com')
+        self.other_site = SiteFactory(domain='other-site.test')
+        self.other_site_org = OrganizationFactory(sites=[self.other_site])
+        self.my_site_org = OrganizationFactory(sites=[self.my_site])
+
+        self.my_courses = [CourseFactory.create() for i in range(0,2)]
+        self.my_course_overviews = [
+            CourseOverviewFactory(id=course.id) for course in self.my_courses
+        ]
+
+        for co in self.my_course_overviews:
+            OrganizationCourseFactory(organization=self.my_site_org,
+                                      course_id=str(co.id))
+
+        self.my_enrollments = [
+            CourseEnrollmentFactory(course=self.my_course_overviews[0]),
+            CourseEnrollmentFactory(course=self.my_course_overviews[1]),
+        ]
+
+        for enrollment in self.my_enrollments:
+            UserOrganizationMappingFactory(user=enrollment.user,
+                                           organization=self.my_site_org)
+
+        self.other_enrollments = [CourseEnrollmentFactory()]
+        # self.other_course_overviews = [CourseOverviewFactory()]
+        OrganizationCourseFactory(organization=self.other_site_org,
+                                  course_id=str(
+                                    self.other_enrollments[0].course_overview.id))
+
+        self.caller = UserFactory()
+        UserOrganizationMappingFactory(user=self.caller,
+                                organization=self.my_site_org,
+                                is_amc_admin=True,
+            )
+
+
+    def test_enroll_learners_single_course(self):
+        """
+        The payload structure is subject to change
+        """
+
+        co = self.my_course_overviews[0]
+        reg_users = [UserFactory(), UserFactory()]
+        new_users = ['alpha@example.com', 'bravo@example.com']
+        # TODO: make sure these emails don't exist
+        learner_emails = [obj.email for obj in reg_users]
+        payload = {
+            'action': 'enroll',
+            'auto_enroll': True,
+            'identifiers': learner_emails,
+            'email_learners': True,
+            'courses': [
+                str(co.id)
+            ],
+        }
+
+
+        factory = APIRequestFactory()
+        url = reverse('tahoe-api:v1:enrollments-list') 
+        request = factory.post(url, payload)
+        request.META['HTTP_HOST'] = self.my_site.domain
+        force_authenticate(request, user=self.caller)
+
+        view = resolve(url).func
+        response = view(request)
+        response.render()
+
+        import pdb; pdb.set_trace()
+
+
+        # res = self.client.post(url, payload)
+        # self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        # for key in ['action', 'auto_enroll', 'email_learners', 'courses']:
+        #     self.assertEqual(res.data[key], payload[key])
+
+        # # TODO: Assert that the course enrollments created
+        # # TODO: Assert new users registered
+        # import pdb; pdb.set_trace()
