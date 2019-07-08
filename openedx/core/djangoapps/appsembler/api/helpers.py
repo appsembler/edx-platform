appsembler/edx-platform

commit 7fbb26a29d216b3fb69932dd60a54967b89fb46c

Multi-site aware enrollments and courses API (#397)
* Enable Django REST Framework browsable API for docker devstack

* WIP Tahoe API courses resource

* WIP appsembler api courses endpoint  testing

* Got course api and site module tests working

* Course list view test fails. Only one course is returned. Otherwise, very close

* Tahoe API v1 courses endpoint with some basic test coverage

* test_course_api module docstring fix

* Tahoe api sites module cleanup

* Tahoe API updated sites and test_sites for enrollments

* Tahoe API WIP enrollments

* WIP Tahoe enrollment api

* Tahoe API cleanup code for test_course_api

* Tahoe API - fixed typo in v1.urls

* Tahoe enrollment api - added stub for enrollment create method

* Fixed missing import in tahoe api v1 filters

* Tahoe API WIP course enrollment api tests

* Tahoe API serializer - Add BulkEnrollmentSerializer

* WIP Tahoe enrollment api

* WIP Tahoe API - enrollment

* tahoe api cleanup

* WIP Tahoe API enrollment, added modulestore support for enrollment api using existing code

* WIP enrollment api

* WIP Tahoe enrollment api test

* WIP tahoe api enrollment - wrapping up testing

* Tahoe API enrollment API tests now passing

* Updated tests to use absolute path for 'factories' import

* Updated test instructions in readme

* Tahoe API - replaced relative imports with absolute imports

* Tahoe enrollment api - addressed PR comments for PR #397, except for the comment on using the default site

* Updated site handling in tests per PR #397 comments

* Disabled throttling test. Getting a transaction error. Need to investigate

* Tahoe enrollment api - Added invalid site check in enrollment POST

openedx/core/djangoapps/appsembler/api/helpers.py
@@ -1,0 +1,19 @@
+
+from opaque_keys.edx.keys import CourseKey
+
+def as_course_key(course_id):
+    '''Returns course id as a CourseKey instance
+
+    Convenience method to return the paramater unchanged if it is of type
+    ``CourseKey`` or attempts to convert to ``CourseKey`` if of type str or
+    unicode.
+
+    Raises TypeError if an unsupported type is provided
+    '''
+    if isinstance(course_id, CourseKey):
+        return course_id
+    elif isinstance(course_id, basestring):
+        return CourseKey.from_string(course_id)
+    else:
+        raise TypeError('Unable to convert course id with type "{}"'.format(
+            type(course_id)))

openedx/core/djangoapps/appsembler/api/v1/urls.py
@@ -7,16 +7,28 @@ from rest_framework import routers
 # Initially doing relative pathing because the full path is a mouthful and a half:
 #  `openedx.core.djangoapps.appsembler.api`
 
-from . import views
+from openedx.core.djangoapps.appsembler.api.v1 import views
+
 
 router = routers.DefaultRouter()
+
+router.register(
+    r'courses',
+    views.CourseViewSet,
+    'courses',
+    )
+
+router.register(
+    r'enrollments',
+    views.EnrollmentViewSet,
+    'enrollments',
+    )
 
 router.register(
     r'registrations',
     views.RegistrationViewSet,
     'registrations',
 )
-
 
 urlpatterns = [
     url(r'', include(router.urls, )),

openedx/core/djangoapps/appsembler/api/v1/views.py
@@ -4,26 +4,73 @@ Only include view classes here. See the tests/test_permissions.py:get_api_classe
 method.
 """
 
+from functools import partial
 import logging
 import random
 import string
 
+
 from django.core.exceptions import NON_FIELD_ERRORS, ValidationError
 from django.conf import settings
 from django.db import transaction
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
+import django.contrib.sites.shortcuts
 
-from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
+
+from rest_framework import status, viewsets
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.exceptions import NotFound
+from rest_framework.filters import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from opaque_keys.edx.keys import CourseKey
+
+from enrollment.serializers import CourseEnrollmentSerializer
+
+# from courseware.courses import get_course_by_id, get_course_with_access
+from courseware.courses import get_course_by_id
+
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 from openedx.core.djangoapps.user_api.accounts.api import check_account_exists
 from student.forms import PasswordResetFormNoActive
+from student.models import CourseEnrollment
 from student.views import create_account_with_params
 
-from ..permissions import IsSiteAdminUser, TahoeAPIUserThrottle
+from lms.djangoapps.instructor.enrollment import (
+    enroll_email,
+    get_email_params,
+    # get_user_email_language,
+    # send_beta_role_email,
+    # send_mail_to_student,
+    # unenroll_email
+)
+
+from openedx.core.djangoapps.appsembler.api.helpers import as_course_key
+from openedx.core.djangoapps.appsembler.api.v1.api import enroll_learners_in_course
+from openedx.core.djangoapps.appsembler.api.v1.filters import (
+    CourseEnrollmentFilter,
+    CourseOverviewFilter
+)
+from openedx.core.djangoapps.appsembler.api.v1.pagination import (
+    TahoeLimitOffsetPagination
+)
+from openedx.core.djangoapps.appsembler.api.v1.serializers import (
+    CourseOverviewSerializer, BulkEnrollmentSerializer
+)
+
+# TODO: Just move into v1 directory
+from openedx.core.djangoapps.appsembler.api.permissions import (
+    IsSiteAdminUser, TahoeAPIUserThrottle
+)
+from openedx.core.djangoapps.appsembler.api.sites import (
+    get_courses_for_site,
+    get_site_for_course,
+    get_enrollments_for_site,
+    course_belongs_to_site,
+)
 
 
 log = logging.getLogger(__name__)
@@ -71,6 +118,7 @@ class TahoeAuthMixin(object):
     """Provides a common authorization base for the Tahoe multi-site aware API views
     """
     authentication_classes = (
+        SessionAuthentication,
         TokenAuthentication,
     )
     permission_classes = (
@@ -170,3 +218,135 @@ class RegistrationViewSet(TahoeAuthMixin, viewsets.ViewSet):
             }
             return Response(errors, status=400)
         return Response({'user_id ': user_id}, status=200)
+
+
+class CourseViewSet(TahoeAuthMixin, viewsets.ReadOnlyModelViewSet):
+    """Provides course information
+
+    To provide data for all courses on your site::
+
+        GET /tahoe/api/v1/courses/
+
+    To provide details on a specific course::
+
+        GET /tahoe/api/v1/courses/<course id>/
+
+    """
+    model = CourseOverview
+    pagination_class = TahoeLimitOffsetPagination
+    serializer_class = CourseOverviewSerializer
+    throttle_classes = (TahoeAPIUserThrottle,)
+    filter_backends = (DjangoFilterBackend, )
+    filter_class = CourseOverviewFilter
+
+    def get_queryset(self):
+        site = django.contrib.sites.shortcuts.get_current_site(self.request)
+        queryset = get_courses_for_site(site)
+        return queryset
+
+    def retrieve(self, request, *args, **kwargs):
+        course_id_str = kwargs.get('pk', '')
+        course_key = as_course_key(course_id_str.replace(' ', '+'))
+        site = django.contrib.sites.shortcuts.get_current_site(request)
+        if site != get_site_for_course(course_key):
+            # Raising NotFound instead of PermissionDenied
+            raise NotFound()
+        course_overview = get_object_or_404(CourseOverview, pk=course_key)
+        return Response(CourseOverviewSerializer(course_overview).data)
+
+
+# @can_disable_rate_limit
+class EnrollmentViewSet(TahoeAuthMixin, viewsets.ModelViewSet):
+    """Provides course information
+
+    To provide data for all enrollments on your site::
+
+        GET /tahoe/api/v1/enrollments/
+
+    To provide enrollments for a specific course::
+
+        GET /tahoe/api/v1/enrollments/<course id>/
+
+    """
+    model = CourseEnrollment
+    pagination_class = TahoeLimitOffsetPagination
+    serializer_class = CourseEnrollmentSerializer
+    throttle_classes = (TahoeAPIUserThrottle,)
+    filter_backends = (DjangoFilterBackend, )
+    filter_class = CourseEnrollmentFilter
+
+    def get_queryset(self):
+        site = django.contrib.sites.shortcuts.get_current_site(self.request)
+        queryset = get_enrollments_for_site(site)
+        return queryset
+
+    def retrieve(self, request, *args, **kwargs):
+        course_id_str = kwargs.get('pk', '')
+        course_key = as_course_key(course_id_str.replace(' ', '+'))
+        site = django.contrib.sites.shortcuts.get_current_site(request)
+        if site != get_site_for_course(course_key):
+            # Raising NotFound instead of PermissionDenied
+            raise NotFound()
+        course_overview = get_object_or_404(CourseOverview, pk=course_key)
+        return Response(CourseOverviewSerializer(course_overview).data)
+
+    def create(self, request, *args, **kwargs):
+        """
+        Adapts interface from bulk enrollment
+
+        """
+        site = django.contrib.sites.shortcuts.get_current_site(request)
+        serializer = BulkEnrollmentSerializer(data=request.data)
+        if serializer.is_valid():
+            # TODO: Follow-on: Wrap in transaction
+            # TODO: trap error on each attempt and log
+            # IMPORTANT: THIS IS A WIP to get this working quickly
+            # Being clean inside is secondary
+
+            invalid_course_ids = [course_id for course_id in serializer.data.get('courses')
+                                  if not course_belongs_to_site(site, course_id)]
+
+            if invalid_course_ids:
+                # Don't do bulk enrollment. Return error message and failing
+                # course ids
+                response_data = {
+                    'error': 'invalid-course-ids',
+                    'invalid_course_ids' : invalid_course_ids,
+                }
+                response_code = status.HTTP_400_BAD_REQUEST
+            else:
+                # Do bulk enrollment
+                email_learners = serializer.data.get('email_learners')
+                identifiers = serializer.data.get('identifiers')
+                auto_enroll = serializer.data.get('auto_enroll')
+                for course_id in serializer.data.get('courses'):
+                    course_key = as_course_key(course_id)
+                    if email_learners:
+                        email_params = get_email_params(course=get_course_by_id(course_key),
+                                                        auto_enroll=auto_enroll,
+                                                        secure=request.is_secure())
+                    else:
+                        email_params = {}
+                    results = enroll_learners_in_course(
+                            course_id=course_key,
+                            identifiers=identifiers,
+                            enroll_func=partial(enroll_email,
+                                                auto_enroll=auto_enroll,
+                                                email_students=email_learners,
+                                                email_params=email_params),
+                            request_user=request.user)
+
+                response_data = {
+                    'auto_enroll': serializer.data.get('auto_enroll'),
+                    'email_learners': serializer.data.get('email_learners'),
+                    'action': serializer.data.get('action'),
+                    'courses': serializer.data.get('courses'),
+                    'results': results,
+                }
+                response_code = status.HTTP_201_CREATED
+        else:
+            # Don't do bulk enrollment. Return serializer error as response body
+            response_data = serializer.errors
+            response_code = status.HTTP_400_BAD_REQUEST
+
+        return Response(response_data, status=response_code)

openedx/core/djangoapps/appsembler/api/tests/factories.py
@@ -1,5 +1,10 @@
-import factory
 
+import datetime
+
+import factory
+from openedx.core.djangoapps.content.course_overviews.models import (
+    CourseOverview,
+)
 from openedx.core.djangoapps.site_configuration.models import SiteConfiguration
 
 from student.tests.factories import UserFactory
@@ -9,6 +14,35 @@ from openedx.core.djangoapps.site_configuration.tests.factories import (
 )
 
 import organizations
+
+from openedx.core.djangoapps.appsembler.api.helpers import as_course_key
+
+
+COURSE_ID_STR_TEMPLATE = 'course-v1:StarFleetAcademy+SFA{}+2161'
+
+
+class CourseOverviewFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = CourseOverview
+
+    # Only define the fields that we will retrieve
+    id = factory.Sequence(lambda n: as_course_key(
+        COURSE_ID_STR_TEMPLATE.format(n)))
+    display_name = factory.Sequence(lambda n: 'SFA Course {}'.format(n))
+    org = 'StarFleetAcademy'
+    version = CourseOverview.VERSION
+    display_org_with_default = factory.LazyAttribute(lambda o: o.org)
+    created = factory.fuzzy.FuzzyDateTime(datetime.datetime(
+        2018, 2, 1, tzinfo=factory.compat.UTC))
+    enrollment_start = factory.fuzzy.FuzzyDateTime(datetime.datetime(
+        2018, 3, 1, tzinfo=factory.compat.UTC))
+    enrollment_end = factory.fuzzy.FuzzyDateTime(datetime.datetime(
+        2018, 3, 15, tzinfo=factory.compat.UTC))
+    start = factory.fuzzy.FuzzyDateTime(datetime.datetime(
+        2018, 4, 1, tzinfo=factory.compat.UTC))
+    end = factory.fuzzy.FuzzyDateTime(datetime.datetime(
+        2018, 6, 1, tzinfo=factory.compat.UTC))
+    self_paced = False
 
 
 class SiteConfigurationFactory(factory.DjangoModelFactory):
@@ -58,3 +92,13 @@ class UserOrganizationMappingFactory(factory.DjangoModelFactory):
     organization = factory.SubFactory(OrganizationFactory)
     is_active = True
     is_amc_admin = False
+
+
+class OrganizationCourseFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = organizations.models.OrganizationCourse
+
+    course_id = factory.Sequence(lambda n: COURSE_ID_STR_TEMPLATE.format(n))
+    organization = factory.SubFactory(OrganizationFactory)
+    active = True
+

openedx/core/djangoapps/appsembler/api/tests/test_course_api.py
@@ -1,0 +1,91 @@
+"""
+Tests for openedx.core.djangoapps.appsembler.api.v1.views.CourseViewSet
+
+These tests adapted from Appsembler enterprise `appsembler_api` tests
+
+"""
+
+# from django.contrib.sites.models import Site
+from django.core.urlresolvers import resolve, reverse
+
+from django.test import RequestFactory, TestCase
+from django.test.utils import override_settings
+
+from rest_framework.permissions import AllowAny
+from rest_framework.test import APIRequestFactory, force_authenticate
+
+import ddt
+import mock
+
+from student.tests.factories import UserFactory
+from openedx.core.djangoapps.site_configuration.tests.factories import (
+    SiteConfigurationFactory,
+    SiteFactory,
+)
+
+
+from openedx.core.djangoapps.appsembler.api.tests.factories import (
+    CourseOverviewFactory,
+    OrganizationFactory,
+    OrganizationCourseFactory,
+    UserOrganizationMappingFactory,
+)
+
+from openedx.core.djangoapps.appsembler.api.v1.views import CourseViewSet
+
+
+APPSEMBLER_API_VIEWS_MODULE = 'openedx.core.djangoapps.appsembler.api.v1.views'
+
+
+@ddt.ddt
+@mock.patch(APPSEMBLER_API_VIEWS_MODULE + '.CourseViewSet.throttle_classes', [])
+class CourseApiTest(TestCase):
+
+    def setUp(self):
+        self.my_site = SiteFactory(domain='my-site.test')
+        self.other_site = SiteFactory(domain='other-site.test')
+        self.other_site_org = OrganizationFactory(sites=[self.other_site])
+        self.my_site_org = OrganizationFactory(sites=[self.my_site])
+
+        self.my_course_overviews = [
+            CourseOverviewFactory(),
+            CourseOverviewFactory()
+        ]
+        for co in self.my_course_overviews:
+            OrganizationCourseFactory(organization=self.my_site_org,
+                                      course_id=str(co.id))
+
+        self.other_course_overviews = [CourseOverviewFactory()]
+        OrganizationCourseFactory(organization=self.other_site_org,
+                                  course_id=str(self.other_course_overviews[0].id))
+
+        self.caller = UserFactory()
+        UserOrganizationMappingFactory(user=self.caller,
+                                       organization=self.my_site_org,
+                                       is_amc_admin=True)
+
+    def test_get_list(self):
+        url = reverse('tahoe-api:v1:courses-list')
+        request = APIRequestFactory().get(url)
+        request.META['HTTP_HOST'] = self.my_site.domain
+        force_authenticate(request, user=self.caller)
+        view = resolve(url).func
+        response = view(request)
+        response.render()
+        results = response.data['results']
+        self.assertEqual(response.status_code, 200)
+        expected_keys = [str(co.id) for co in self.my_course_overviews]
+        self.assertEqual(set([obj['id'] for obj in results]), set(expected_keys) )
+
+    def test_get_single(self):
+        course_id = str(self.my_course_overviews[0].id)
+        url = reverse('tahoe-api:v1:courses-detail', args=[course_id])
+        request = APIRequestFactory().get(url)
+        request.META['HTTP_HOST'] = self.my_site.domain
+        force_authenticate(request, user=self.caller)
+        view = resolve(url)
+        response = view.func(request, pk=course_id)
+        response.render()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['id'], course_id)

commit f2daf919a327b7249914808b7cda9a0c236b3bce

fix some pep8 regressions
It looks like 7fbb26a29d216b3fb69932dd60a54967b89fb46c introduced some
pep8 errors. This should clean those up.

openedx/core/djangoapps/appsembler/api/helpers.py
@@ -1,5 +1,5 @@
-
 from opaque_keys.edx.keys import CourseKey
+
 
 def as_course_key(course_id):
     '''Returns course id as a CourseKey instance

openedx/core/djangoapps/appsembler/api/v1/urls.py
@@ -16,13 +16,13 @@ router.register(
     r'courses',
     views.CourseViewSet,
     'courses',
-    )
+)
 
 router.register(
     r'enrollments',
     views.EnrollmentViewSet,
     'enrollments',
-    )
+)
 
 router.register(
     r'registrations',

openedx/core/djangoapps/appsembler/api/v1/views.py
@@ -311,7 +311,7 @@ class EnrollmentViewSet(TahoeAuthMixin, viewsets.ModelViewSet):
                 # course ids
                 response_data = {
                     'error': 'invalid-course-ids',
-                    'invalid_course_ids' : invalid_course_ids,
+                    'invalid_course_ids': invalid_course_ids,
                 }
                 response_code = status.HTTP_400_BAD_REQUEST
             else:
@@ -328,13 +328,13 @@ class EnrollmentViewSet(TahoeAuthMixin, viewsets.ModelViewSet):
                     else:
                         email_params = {}
                     results = enroll_learners_in_course(
-                            course_id=course_key,
-                            identifiers=identifiers,
-                            enroll_func=partial(enroll_email,
-                                                auto_enroll=auto_enroll,
-                                                email_students=email_learners,
-                                                email_params=email_params),
-                            request_user=request.user)
+                        course_id=course_key,
+                        identifiers=identifiers,
+                        enroll_func=partial(enroll_email,
+                                            auto_enroll=auto_enroll,
+                                            email_students=email_learners,
+                                            email_params=email_params),
+                        request_user=request.user)
 
                 response_data = {
                     'auto_enroll': serializer.data.get('auto_enroll'),

openedx/core/djangoapps/appsembler/api/tests/factories.py
@@ -101,4 +101,3 @@ class OrganizationCourseFactory(factory.DjangoModelFactory):
     course_id = factory.Sequence(lambda n: COURSE_ID_STR_TEMPLATE.format(n))
     organization = factory.SubFactory(OrganizationFactory)
     active = True
-

openedx/core/djangoapps/appsembler/api/tests/test_course_api.py
@@ -75,7 +75,7 @@ class CourseApiTest(TestCase):
         results = response.data['results']
         self.assertEqual(response.status_code, 200)
         expected_keys = [str(co.id) for co in self.my_course_overviews]
-        self.assertEqual(set([obj['id'] for obj in results]), set(expected_keys) )
+        self.assertEqual(set([obj['id'] for obj in results]), set(expected_keys))
 
     def test_get_single(self):
         course_id = str(self.my_course_overviews[0].id)

openedx/core/djangoapps/appsembler/api/tests/test_enrollment_api.py
@@ -56,7 +56,7 @@ class EnrollmentApiGetTest(ModuleStoreTestCase):
         self.other_site_org = OrganizationFactory(sites=[self.other_site])
         self.my_site_org = OrganizationFactory(sites=[self.my_site])
 
-        self.my_courses = [CourseFactory.create() for i in range(0,2)]
+        self.my_courses = [CourseFactory.create() for i in range(0, 2)]
         self.my_course_overviews = [
             CourseOverviewFactory(id=course.id) for course in self.my_courses
         ]
@@ -77,7 +77,7 @@ class EnrollmentApiGetTest(ModuleStoreTestCase):
         self.other_enrollments = [CourseEnrollmentFactory()]
         OrganizationCourseFactory(organization=self.other_site_org,
                                   course_id=str(
-                                    self.other_enrollments[0].course_overview.id))
+                                      self.other_enrollments[0].course_overview.id))
 
         self.caller = UserFactory()
         UserOrganizationMappingFactory(user=self.caller,
@@ -129,7 +129,6 @@ class EnrollmentApiGetTest(ModuleStoreTestCase):
     def test_get_single_enrollment(self):
         pass
 
-
     def test_invalid_enroll_data_no_learners(self):
         """
         This does a partial test
@@ -174,7 +173,6 @@ class EnrollmentApiGetTest(ModuleStoreTestCase):
         response = view(request)
         response.render()
 
-
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
@@ -189,7 +187,7 @@ class EnrollmentApiPostTest(ModuleStoreTestCase):
         self.other_site_org = OrganizationFactory(sites=[self.other_site])
         self.my_site_org = OrganizationFactory(sites=[self.my_site])
 
-        self.my_courses = [CourseFactory.create() for i in range(0,2)]
+        self.my_courses = [CourseFactory.create() for i in range(0, 2)]
         self.my_course_overviews = [
             CourseOverviewFactory(id=course.id) for course in self.my_courses
         ]
@@ -210,7 +208,7 @@ class EnrollmentApiPostTest(ModuleStoreTestCase):
         self.other_enrollments = [CourseEnrollmentFactory()]
         OrganizationCourseFactory(organization=self.other_site_org,
                                   course_id=str(
-                                    self.other_enrollments[0].course_overview.id))
+                                      self.other_enrollments[0].course_overview.id))
 
         self.caller = UserFactory()
         UserOrganizationMappingFactory(user=self.caller,
@@ -244,7 +242,7 @@ class EnrollmentApiPostTest(ModuleStoreTestCase):
         for user_email in new_users:
             assert not CourseEnrollmentAllowed.objects.filter(email=user_email).exists()
 
-        url = reverse('tahoe-api:v1:enrollments-list') 
+        url = reverse('tahoe-api:v1:enrollments-list')
         request = APIRequestFactory().post(url, payload)
         request.META['HTTP_HOST'] = self.my_site.domain
         force_authenticate(request, user=self.caller)
@@ -287,7 +285,7 @@ class EnrollmentApiPostTest(ModuleStoreTestCase):
         assert CourseEnrollmentAllowed.objects.count() == len(new_users)
 
         for rec in results:
-            assert not rec.has_key('error')
+            assert 'error' not in rec
             if rec['identifier'] in new_users:
                 assert CourseEnrollmentAllowed.objects.filter(
                     email=rec['identifier']).exists()

openedx/core/djangoapps/appsembler/api/tests/test_sites.py
@@ -32,7 +32,7 @@ from openedx.core.djangoapps.appsembler.api.tests.factories import (
     OrganizationFactory,
     OrganizationCourseFactory,
     UserOrganizationMappingFactory,
-    )
+)
 
 
 class SitesModuleTests(TestCase):
@@ -79,16 +79,17 @@ class SitesModuleTests(TestCase):
     def test_my_course_belongs_to_my_site(self):
         for co in self.my_course_overviews:
             assert aapi_sites.course_belongs_to_site(site=self.my_site,
-                                                    course_id=co.id)
+                                                     course_id=co.id)
+
     def test_my_course_not_belongs_to_other_site(self):
         for co in self.my_course_overviews:
             assert not aapi_sites.course_belongs_to_site(site=self.other_site,
-                                                        course_id=co.id)
+                                                         course_id=co.id)
 
     def test_other_course_not_belongs_to_my_site(self):
         for co in self.other_course_overviews:
             assert not aapi_sites.course_belongs_to_site(site=self.my_site,
-                                                        course_id=co.id)
+                                                         course_id=co.id)
 
     def test_invalid_course_not_belongs_to_site(self):
         for site in Site.objects.all():