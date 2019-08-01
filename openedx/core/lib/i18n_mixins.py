db6d4e6450c4c8e563b6a8d1158c4634effc4103
Omar Al-Ithawi <i@omardo.com>
2019-08-01 15:34:01 +0300

    it works now

--- openedx/core/lib/i18n_mixins.py
+++ openedx/core/lib/i18n_mixins.py
@@ -6,18 +6,18 @@ class TranslatableXBlockMixin(XBlockMixin):
 
     def __init__(self, runtime, *args, **kwargs):
         from xblock.fields import UNSET, UNIQUE_ID, Field
-        from django.utils.translation import ugettext
 
         def apply(f, prop, func):
             try:
                 raw = getattr(f, prop)
                 setattr(f, prop, func(raw))
-            except AttributeError:
+            except AttributeError as e:
+                print 'AttributeError i18n', e
                 pass
 
         def trans(s):
-            if s is not None and isinstance(s, basestring) and s is not UNSET and s is not UNIQUE_ID:
-                return ugettext(s)
+            if s and isinstance(s, basestring) and s is not UNSET and s is not UNIQUE_ID:
+                return self.ugettext(s)
             return s
 
         def trans_struct(s):
@@ -32,39 +32,11 @@ class TranslatableXBlockMixin(XBlockMixin):
 
         super(TranslatableXBlockMixin, self).__init__(runtime, *args, **kwargs)
 
-        if hasattr(self, 'editable_fields'):
+        if hasattr(self, 'editable_fields') or True:
             # for field_name in getattr(self, 'editable_fields', []):
             #     field = self.fields[field_name]
             for _field_name, field in self.fields.iteritems():
-                # apply(field, '_display_name', trans)
-                # apply(field, '_default', trans_struct)
-                # apply(field, 'help', trans)
-                # apply(field, '_values', trans_struct)
-                try:
-                    field._display_name = trans(field._display_name)
-                except AttributeError as e:
-                    print field, e
-                    pass
-
-                try:
-                    field._default = trans_struct(field._default)
-                except AttributeError as e:
-                    print field, e
-                    pass
-
-                try:
-                    field._values = trans_struct(field._values)
-                except AttributeError as e:
-                    print field, e
-                    pass
-
-                try:
-                    field.help = trans(field.help)
-                except AttributeError as e:
-                    print field, e
-                    pass
-                # field._display_name = trans(field._display_name)
-                # # field._default = trans_struct(field._default)
-                # # field._default = trans(field._default)
-                # # field.help = trans(field.help)
-                # # field._values = trans_struct(field._values)
+                apply(field, '_display_name', trans)
+                apply(field, '_default', trans_struct)
+                apply(field, 'help', trans)
+                apply(field, '_values', trans_struct)
