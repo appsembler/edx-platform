605ec455815adc47daaa7e3ae9a7cf4194654d66
John Baldwin <jlbaldwin@gmail.com>
2021-04-21 21:55:36 +0200

    Juniper upgrade - Fix handling to delete Oauth2 tokens
    This commit calls the RefreshToken.revoke() method instead of explicitly
    deleting access tokens and refresh tokens

--- openedx/core/djangoapps/appsembler/auth/oauth.py
+++ openedx/core/djangoapps/appsembler/auth/oauth.py
@@ -4,9 +4,7 @@
 from django.conf import settings
 from django.db.models import Subquery
 
-from oauth2_provider.models import (get_application_model,
-                                    AccessToken,
-                                    RefreshToken)
+from oauth2_provider.models import get_application_model, RefreshToken
 
 from .models import TrustedApplication
 
@@ -20,23 +18,21 @@ def destroy_oauth_tokens(user):
     trusted application tokens should be preserved. These are tracked in the
     `openedx.core.djangoapps.appsembler.auth.models.TrustedApplication` model
     """
-    dot_access_query = AccessToken.objects.filter(user=user.id)
     dot_refresh_query = RefreshToken.objects.filter(user=user.id)
-
-    Application = get_application_model()
 
     if settings.FEATURES.get('KEEP_TRUSTED_CONFIDENTIAL_CLIENT_TOKENS', False):
         # Appsembler: Avoid deleting the trusted confidential applications such
         # as the Appsembler Management Console
-        trusted_applications = Application.objects.filter(
+        trusted_applications = get_application_model().objects.filter(
             client_type=Application.CLIENT_CONFIDENTIAL,
             pk__in=Subquery(TrustedApplication.objects.all().values('id')),
         )
-
-        dot_access_query = dot_access_query.exclude(
-            application__in=trusted_applications)
+        # We could just merge the `trusted_application` into the following,
+        # but keeping them apart makes it a bit easier for future debugging
         dot_refresh_query = dot_refresh_query.exclude(
             application__in=trusted_applications)
 
-    dot_access_query.delete()
-    dot_refresh_query.delete()
+    # The following revokes each token found. The revoke() call deletes the
+    # related access token and marks the refresh token as revoked and marked for
+    # deletion the next time oauth2_provider.models.clear_expired() is called
+    [refresh_token.revoke() for refresh_token in dot_refresh_query]
