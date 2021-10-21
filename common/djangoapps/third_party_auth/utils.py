52abf1af6d4714839733529ae0fe9f5cef6ae99b
Maxi Fernandez <melvinsoft@gmail.com>
2021-10-21 18:57:07 +0000

    fix third party auth and MTE function

--- common/djangoapps/third_party_auth/utils.py
+++ common/djangoapps/third_party_auth/utils.py
@@ -47,21 +47,8 @@ def user_exists(details):
                     user = User.objects.get(username=username)
                 except User.DoesNotExist:
                     return False
-
-                if get_single_user_organization(user) == current_org:
-                    return True
                 else:
-                    logger.info(
-                        'SAMLUnusableUsernameDueToMTE: Cannot use username '
-                        '(%s) in (%s) because it is already taken in a '
-                        'different organization (%s)',
-                        username,
-                        current_org.short_name,
-                        get_single_user_organization(user)
-                    )
-                    raise SAMLUnusableUsernameDueToMTE(
-                        'The username is already taken in a different organization'
-                    )
+                    return True
 
         else:
             return User.objects.filter(**user_queryset_filter).exists()
