ebca58e7e73a157e5a3d87b5dea483209b35f4a1
Shadi Naif <shadinaif@gmail.com>
2022-09-08 09:53:09 +0300

    Set TAHOE_SITES_USE_ORGS_MODELS to test unavailability of the fork

--- openedx/core/djangoapps/appsembler/settings/settings/test_common.py
+++ openedx/core/djangoapps/appsembler/settings/settings/test_common.py
@@ -38,3 +38,5 @@ def plugin_settings(settings):
             'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         }
     })
+
+    settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'] = False
