79095f8f4baf2b128e5fb7748830550bc7df9fc0
Shadi Naif <shadinaif@gmail.com>
2022-09-13 23:29:10 +0300

    After Code Merge 1/6: Use DEFAULT_REDIRECT_AFTER_LOGIN

--- common/djangoapps/student/tests/test_helpers.py
+++ common/djangoapps/student/tests/test_helpers.py
@@ -149,12 +149,12 @@ class TestLoginHelper(TestCase):
         assert '/dashboard' == get_next_url_for_login_page(request), 'Default should be /dashboard'
 
         with with_site_configuration_context(configuration={
-            'LOGIN_REDIRECT_URL': '/about'
+            'DEFAULT_REDIRECT_AFTER_LOGIN': 'about'
         }):
             assert '/about' == get_next_url_for_login_page(request), 'Custom redirect should be used'
 
         with with_site_configuration_context(configuration={
-            'LOGIN_REDIRECT_URL': ''  # Falsy or empty URLs should not be used
+            'DEFAULT_REDIRECT_AFTER_LOGIN': ''  # Falsy or empty URLs should not be used
         }):
             assert '/dashboard' == get_next_url_for_login_page(request), 'Falsy url should default to dashboard'
 
