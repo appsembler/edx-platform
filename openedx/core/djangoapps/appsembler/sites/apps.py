196c141f2b72c0529ed4e8dfe4dff250fb203cbc
Bryan Wilson <bryanlandia@gmail.com>
2023-12-11 13:55:00 -0800

    Add alternative domains to ALLOWED_HOSTS
    need to allow requests from customer custom domains

--- openedx/core/djangoapps/appsembler/sites/apps.py
+++ openedx/core/djangoapps/appsembler/sites/apps.py
@@ -12,11 +12,20 @@ class SitesConfig(AppConfig):
         from openedx.core.djangoapps.site_configuration.models import SiteConfiguration
         from django.contrib.sites.models import Site
         from django.conf import settings
+        from django.core.exceptions import ObjectDoesNotExist
 
         from .config_values_modifier import init_configuration_modifier_for_site_config
 
         pre_save.connect(patched_clear_site_cache, sender=SiteConfiguration)
         post_init.connect(init_configuration_modifier_for_site_config, sender=SiteConfiguration)
         # Update ALLOWED_HOSTS based on Site model
-        site_domains = [site.domain for site in Site.objects.all()]
+        site_domains = []
+        sites = Site.objects.all()
+        for site in sites:
+            site_domains.append(site.domain)
+            try:
+                alt_domain = site.alternative_domain
+                site_domains.append(alt_domain)
+            except ObjectDoesNotExist:
+                continue
         settings.ALLOWED_HOSTS.extend(site_domains)
